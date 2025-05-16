c88facdc6aa11202ea2f3b478429d59bbad98e33
lachelinn <emresynsd@gmail.com>
2025-05-16 23:43:21 +0300

    Add freesqldatabase connection config

--- app.py
+++ app.py
@@ -11,10 +11,11 @@ app = Flask(__name__)
 app.secret_key = 'gizli-anahtar'
 
 # MySQL ayarları
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'emre147a'
-app.config['MYSQL_DB'] = 'ogrenci_db'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASS')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))
 
 # Fotoğraf yükleme ayarları
 UPLOAD_FOLDER = 'static/uploads'
